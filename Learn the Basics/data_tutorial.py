8844ecc797493a88c5408212288223ec0a6dda02
gongyuxia <gongyuxia25@163.com>
2023-06-05 22:06:28 +0800

    修改

--- Learn the Basics/data_tutorial.py
+++ Learn the Basics/data_tutorial.py
@@ -1,25 +1,25 @@
 """
-`Learn the Basics <intro.html>`_ ||
-`Quickstart <quickstart_tutorial.html>`_ ||
-`Tensors <tensorqs_tutorial.html>`_ ||
-**Datasets & DataLoaders** ||
-`Transforms <transforms_tutorial.html>`_ ||
-`Build Model <buildmodel_tutorial.html>`_ ||
-`Autograd <autogradqs_tutorial.html>`_ ||
-`Optimization <optimization_tutorial.html>`_ ||
-`Save & Load Model <saveloadrun_tutorial.html>`_
+`学习基础知识 <intro.html>`_ ||
+`快速开始 <quickstart_tutorial.html>`_ ||
+`张量 <tensorqs_tutorial.html>`_ ||
+`数据集和数据加载器 <data_tutorial.html>`_ ||
+`变换  <transforms_tutorial.html>`_ ||
+`创建模型 <buildmodel_tutorial.html>`_ ||
+**自动求导** ||
+`优化 <optimization_tutorial.html>`_ ||
+`保存和加载模型 <saveloadrun_tutorial.html>`_
 
-Datasets & DataLoaders
+数据集和数据加载器
 ===================
 
 """
 
 #################################################################
-# 在做数据集处理工作时，处理数据样本的代码可能会变得杂乱无章且难以维护。为了提高数据集代码的可读性和规范数据集代码,我们将我们的数据集代码能够与我们的模型训练代码解耦。
-# PyTorch提供了两个数据基元：``torch.utils.data.DataLoader``和``torch.utils.data.Dataset``，这两个数据基元允许你使用预先加载的数据集以及你自己的数据。
-# ``Dataset``存储了样本及其相应的标签，而``DataLoader``在``Dataset``周围包裹了一个可迭代的数据集，以便能够方便地访问这些样本。
+# 在做数据集处理工作时，处理数据样本的代码可能会变得杂乱无章且难以维护。为了提高数据集代码的可读性和规范数据集代码，我们将我们的数据集代码能够与我们的模型训练代码解耦。
+# PyTorch提供了两个数据基元：``torch.utils.data.DataLoader`` 和 ``torch.utils.data.Dataset`` ，这两个数据基元允许你使用预先加载的数据集以及你自己的数据。
+# ``Dataset`` 存储了样本及其相应的标签，而 ``DataLoader`` 在 ``Dataset`` 周围包裹了一个可迭代的数据集，以便能够方便地访问这些样本。
 #
-# PyTorch领域库提供了一些子类为``torch.utils.data.Dataset``的预加载的数据集（如FashionMNIST），并实现了针对特定数据的功能。这些可以用来为你的模型建立原型和基准。
+# PyTorch领域库提供了一些子类为 ``torch.utils.data.Dataset`` 的预加载的数据集（如FashionMNIST），并实现了针对特定数据的功能。这些可以用来为你的模型建立原型和基准。
 # 你可以在这里找到它们: `Image Datasets <https://pytorch.org/vision/stable/datasets.html>`_、
 # `Text Datasets  <https://pytorch.org/text/stable/datasets.html>`_ 和
 # `Audio Datasets <https://pytorch.org/audio/stable/datasets.html>`_
@@ -35,7 +35,7 @@ Datasets & DataLoaders
 # 我们用以下参数加载 `FashionMNIST Dataset <https://pytorch.org/vision/stable/datasets.html#fashion-mnist>`_ :
 #  - ``root`` 是存储训练/测试数据的路径。
 #  - ``train`` 指定训练或测试数据集。
-#  - ``download=True`` 如果``root``下没有数据，则从网上下载。
+#  - ``download=True`` 如果 ``root`` 下没有数据，则从网上下载。
 #  - ``transform`` 和 ``target_transform`` 指定特征和标签的转换。
 
 
@@ -65,7 +65,7 @@ test_data = datasets.FashionMNIST(
 # 迭代和可视化数据集
 # -----------------
 #
-# 我们可以像列表一样手动索引 ``Datasets`` : ``training_data[index]``。
+# 我们可以像列表一样手动索引 ``Datasets`` : ``training_data[index]`` 。
 # 我们使用 ``matplotlib`` 来可视化训练数据中的一些样本。
 
 labels_map = {
@@ -174,9 +174,9 @@ def __len__(self):
 # __getitem__
 # ^^^^^^^^^^^^^^^^^^^^
 #
-# __getitem__ 函数从数据集中给定的下标index``idx``加载并返回一个样本。
-# 基于这个下标，该函数可以识别图像的存储位置,将图像的存储位置用``read_image``函数转换为一个张量,
-# 并在``self.img_labels``中的csv数据检索相应的标签，对他们调用变换（transform）
+# __getitem__ 函数从数据集中给定的下标index ``idx`` 加载并返回一个样本。
+# 基于这个下标，该函数可以识别图像的存储位置,将图像的存储位置用 ``read_image`` 函数转换为一个张量,
+# 并在 ``self.img_labels`` 中的csv数据检索相应的标签，对他们调用变换（transform）
 # 函数（如果适合的话），并在一个元组中返回张量图和相应的标签。
 
 def __getitem__(self, idx):
@@ -198,8 +198,8 @@ def __getitem__(self, idx):
 #################################################################
 # 使用DataLoaders为训练准备数据
 # -------------------------------------------------
-# ``Dataset`` 一次一个样本地检索我们数据集的特征和标签。 在训练模型时，我们通常希望以"minibatches"的形式传递样本，
-# 在每个epoch打乱数据以减少模型的过拟合，并使用python的``multiprocessing``来加速数据检索。
+# ``Dataset`` 一次一个样本地检索我们数据集的特征和标签。 在训练模型时，我们通常希望以"迷你批"的形式传递样本，
+# 在每个历时打乱数据以减少模型的过拟合，并使用python的 ``multiprocessing`` 来加速数据检索。
 #
 # ``DataLoader`` 是一个可迭代的对象，它用一个简单的API为我们抽象了这种复杂性。
 
@@ -213,7 +213,7 @@ test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
 # --------------------------
 #
 # 我们已经将该数据集加载到 ``DataLoader`` 中，并可以根据需要迭代该数据集。
-# 下面的每次迭代都会返回一批 ``train_features`` 和 ``train_labels`` (分别包括``batch_size=64``特征和标签)。
+# 下面的每次迭代都会返回一批 ``train_features`` 和 ``train_labels`` (分别包括 ``batch_size=64`` 特征和标签)。
 # 因为我们指定了shuffle=True，所以在我们遍历所有批次后，数据会被打乱（要想对数据加载顺序进行更精细的控制，
 # 请看 `Samplers <https://pytorch.org/docs/stable/data.html#data-loading-order-and-sampler>`_)。
 
